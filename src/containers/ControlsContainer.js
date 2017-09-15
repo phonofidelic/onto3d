a7ed62239f45be5f90d0095d32c3127d71ea7867
Christopher Clemons <chris.clemons85@gmail.com>
2017-09-14 19:31:44 -0700

    fix: hide browser navigation menu (safari, iOS)
    Added meta tag to hide browser navigation ui.
    Only render controls ui if user is not in vr mode.

--- src/containers/ControlsContainer.js
+++ src/containers/ControlsContainer.js
@@ -12,32 +12,37 @@ class ControlsContainer extends Component {
 	}
 
 	render() {
-		const { shapeList } = this.props;
-		return (
-			<div className="controlls-container container">
-				<div style={{height: '50px'}}>
-					Controlls container
-					<span className="enter-vr-control"><button type="button" className="btn btn-default" onClick={() => {this.props.enterVr()}}>Enter VR</button></span>
-				</div>
-				{ shapeList.map((shape, i) => {
-					if (shape.selected === true) {
-						return (
-							<ShapeControl id={shape.id} shape={shape} handleShapeChange={this.handleShapeChange.bind(this)} key={i} />
-						)
-					} else {
-						return null;
-					}
-				})}
+		const { shapeList, vrMode } = this.props;
 
-			</div>
-		);
+		if (!vrMode) {
+			return (
+				<div className="controlls-container container">
+					<div style={{height: '50px'}}>
+						Controlls container
+					</div>
+					{ shapeList.map((shape, i) => {
+						if (shape.selected) {
+							return (
+								<ShapeControl id={shape.id} shape={shape} handleShapeChange={this.handleShapeChange.bind(this)} key={i} />
+							)
+						} else {
+							return null;
+						}
+					})}
+
+				</div>
+			);
+		} else {
+			return null;
+		}
 	}
 }
 
 const mapStateToProps = state => {
 	console.log('ControlsContainer, state:', state)
 	return {
-		shapeList: state.shapeListReducer.shapeList
+		shapeList: state.shapeListReducer.shapeList,
+		vrMode: state.sceneReducer.vrMode
 	}
 }
 
